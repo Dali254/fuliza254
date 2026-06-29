11ca142b30930dcf0596443fd221847520882dc3
Dali254 <dakatjk@gmail.com>
2026-06-29 21:30:08 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 // Vercel entry point — hands every /api request to the Express app in app.js.
 // (On cPanel or a normal server you don't use this file; you run app.js directly.)
-module.exports = require("../app.js");
+module.exports = require("/app.js");
